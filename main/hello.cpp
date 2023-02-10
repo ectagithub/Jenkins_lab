2bc6290ebeaab54faf7a8b8c217f528081f4d3f6
ectagithub <124993553+ectagithub@users.noreply.github.com>
2023-02-10 12:45:18 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+
+  cout << "I have successfully built and run\n";
+  cout<< "Webhook successfully enabled\n";
+  return 0;
+}
