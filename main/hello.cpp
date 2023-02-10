bcae00150075bde9809023a2be3e6e4f07465679
ectagithub <124993553+ectagithub@users.noreply.github.com>
2023-02-10 12:46:36 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-
   cout << "I have successfully built and run\n";
-  cout<< "Webhook successfully enabled\n";
   return 0;
 }
